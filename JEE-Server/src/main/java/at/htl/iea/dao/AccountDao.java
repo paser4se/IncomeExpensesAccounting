fe4dd54e517c9d771d010783239b916ae0218d39
JulianNobis <julian09022001@gmail.com>
2019-03-05 09:47:46 +0100

    added implemenentation for Account Management

--- JEE-Server/src/main/java/at/htl/iea/dao/AccountDao.java
+++ JEE-Server/src/main/java/at/htl/iea/dao/AccountDao.java
@@ -2,19 +2,28 @@ package at.htl.iea.dao;
 
 import at.htl.iea.model.Account;
 
-import javax.ejb.Singleton;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 @Stateless
-//@Singleton
 public class AccountDao {
     @PersistenceContext
     EntityManager em;
 
-    public Account getAccountByUsername(String username){
-        return em.createNamedQuery("Account.getAccountByUsername", Account.class).setParameter(1, username).getSingleResult();
+    public Account getAccountByUsername(String username) {
+        return em.createNamedQuery("Account.getAccountByUsername", Account.class).setParameter(1, username)
+                .getSingleResult();
+    }
+
+    public Account getAccountByEmail(String email){
+        return em.createNamedQuery("Account.getAccountByEmail", Account.class).setParameter(1, email)
+                .getSingleResult();
+    }
+
+    public void persist(Account account) {
+        em.persist(account);
+        em.flush();
     }
 
 }
--- JEE-Server/src/main/java/at/htl/iea/model/Account.java
+++ JEE-Server/src/main/java/at/htl/iea/model/Account.java
@@ -7,21 +7,29 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 @Entity
 @XmlRootElement
-@NamedQueries(
-        @NamedQuery(name = "Account.getAccountByUsername", query = "select a from Account a where a.username = ?1")
-)
+@NamedQueries({
+        @NamedQuery(name = "Account.getAccountByUsername", query = "select a from Account a where a.username = ?1"),
+        @NamedQuery(name = "Account.getAccountByEmail", query = "select a from Account a where a.email = ?1")
+})
 public class Account {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(unique = true)
     private String username;
     private String password;
+    @Column(unique = true)
     @Email(message = "Email should be valid")
     private String email;
     private String fullName;
 
     // region Constructor
     public Account(){}
+    public Account(String username, String password, String fullName, String email){
+        this.username = username;
+        this.password = password;
+        this.fullName = fullName;
+        this.email = email;
+    }
     // endregion
 
     // region Getter & Setter
