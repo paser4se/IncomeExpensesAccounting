4b21ef7de7d0b1d3707c2e1cac770b241db4bc51
JulianNobis <julian09022001@gmail.com>
2019-03-05 09:48:15 +0100

    added sign up/sign in functionality

--- webserver/wwwroot/src/components/my-app.js
+++ webserver/wwwroot/src/components/my-app.js
@@ -8,7 +8,17 @@ Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
 */
 
-<<<<<<< HEAD
+/*import { LitElement, html } from "@polymer/lit-element";
+import { setPassiveTouchGestures } from "@polymer/polymer/lib/utils/settings.js";
+import { connect } from "pwa-helpers/connect-mixin.js";
+import { installMediaQueryWatcher } from "pwa-helpers/media-query.js";
+import { installOfflineWatcher } from "pwa-helpers/network.js";
+import { installRouter } from "pwa-helpers/router.js";
+import { updateMetadata } from "pwa-helpers/metadata.js";
+import { createStore } from "redux";*/
+
+import "@polymer/iron-icon/iron-icon.js";
+import "@polymer/iron-icons/iron-icons.js";
 import { LitElement, html } from "@polymer/lit-element";
 import { setPassiveTouchGestures } from "@polymer/polymer/lib/utils/settings.js";
 import { connect } from "pwa-helpers/connect-mixin.js";
@@ -16,26 +26,13 @@ import { installMediaQueryWatcher } from "pwa-helpers/media-query.js";
 import { installOfflineWatcher } from "pwa-helpers/network.js";
 import { installRouter } from "pwa-helpers/router.js";
 import { updateMetadata } from "pwa-helpers/metadata.js";
-import { createStore } from "redux";
+import { FooterStyle } from "./footer-style";
+import { AppStyle } from "./app-style";
 
 import "@polymer/iron-icon/iron-icon.js";
 import "@polymer/iron-icons/iron-icons.js";
-=======
-import { LitElement, html } from '@polymer/lit-element';
-import { setPassiveTouchGestures } from '@polymer/polymer/lib/utils/settings.js';
-import { connect } from 'pwa-helpers/connect-mixin.js';
-import { installMediaQueryWatcher } from 'pwa-helpers/media-query.js';
-import { installOfflineWatcher } from 'pwa-helpers/network.js';
-import { installRouter } from 'pwa-helpers/router.js';
-import { updateMetadata } from 'pwa-helpers/metadata.js';
-import { FooterStyle } from "./footer-style";
-import {AppStyle} from "./app-style";
-
-import '@polymer/iron-icon/iron-icon.js';
-import '@polymer/iron-icons/iron-icons.js';
-import '@polymer/iron-icons/editor-icons.js';
-import '@polymer/iron-icons/social-icons.js';
->>>>>>> 236c719ad45f1b5f2d1db75fb4f22fc2043074cb
+import "@polymer/iron-icons/editor-icons.js";
+import "@polymer/iron-icons/social-icons.js";
 
 // This element is connected to the Redux store.
 import { store } from "../store.js";
@@ -54,232 +51,9 @@ class IEA extends connect(store)(LitElement) {
   render() {
     // Anything that's related to rendering should be done in here.
     return html`
-<<<<<<< HEAD
-      <style>
-        :host {
-          --app-drawer-width: 256px;
-          display: block;
-
-          --app-primary-color: #288b9e;
-          --app-secondary-color: rgb(41, 50, 55);
-          --app-dark-text-color: var(--app-secondary-color);
-          --app-light-text-color: white;
-          --app-section-even-color: #f7f7f7;
-          --app-section-odd-color: white;
-
-          --app-header-background-color: white;
-          --app-header-text-color: var(--app-dark-text-color);
-          --app-header-selected-color: var(--app-primary-color);
-
-          --app-drawer-background-color: var(--app-secondary-color);
-          --app-drawer-text-color: var(--app-light-text-color);
-          --app-drawer-selected-color: #288b9e;
-        }
-
-        app-header {
-          position: fixed;
-          top: 0;
-          left: 0;
-          width: 100%;
-          text-align: center;
-          background-color: var(--app-header-background-color);
-          color: var(--app-header-text-color);
-          border-bottom: 1px solid #eee;
-        }
-
-        .toolbar-top {
-          background-color: var(--app-header-background-color);
-        }
-
-        [main-title] {
-          color: var(--app-primary-color);
-          padding-top: 20px;
-          font-family: "Dyuthi";
-          font-weight: bold;
-          font-size: 80px;
-          /* In the narrow layout, the toolbar is offset by the width of the
-        drawer button, and the text looks not centered. Add a padding to
-        match that button */
-          padding-right: 44px;
-        }
-
-        [left-item] {
-          float: left !important;
-        }
-
-        [right-item] {
-          float: right !important;
-        }
-
-        .toolbar-list {
-          display: none;
-          text-align: left !important;
-        }
-
-        .toolbar-list > a {
-          display: inline-block;
-          color: var(--app-header-text-color);
-          text-decoration: none;
-          line-height: 30px;
-          padding: 4px 24px;
-          outline: 0;
-        }
-
-        .toolbar-list > a[selected] {
-          /*color: var(--app-header-selected-color);*/
-          border-bottom: 4px solid var(--app-header-selected-color);
-
-          outline: 0;
-        }
-
-        .menu-btn {
-          background: none;
-          border: none;
-          fill: var(--app-header-text-color);
-          cursor: pointer;
-          height: 44px;
-          width: 44px;
-        }
-
-        .drawer-list {
-          box-sizing: border-box;
-          width: 100%;
-          height: 100%;
-          padding: 24px;
-          background: var(--app-drawer-background-color);
-          position: relative;
-        }
-
-        .drawer-list > a {
-          display: block;
-          text-decoration: none;
-          color: var(--app-drawer-text-color);
-          line-height: 40px;
-          padding: 0 24px;
-          outline: 0;
-        }
-
-        .drawer-list > a[selected] {
-          color: var(--app-drawer-selected-color);
-          outline: 0;
-        }
-
-        /* Workaround for IE11 displaying <main> as inline */
-        main {
-          display: block;
-        }
-
-        .main-content {
-          padding-top: 64px;
-          min-height: calc(100vh - 64px - 104px);
-        }
-
-        .page {
-          display: none;
-        }
-
-        .page[active] {
-          display: block;
-        }
-
-        footer {
-          padding: 24px;
-          background: var(--app-drawer-background-color);
-          color: var(--app-drawer-text-color);
-          text-align: center;
-        }
-
-        nav.pullUp a:before,
-        nav.pullUp div.profile:before {
-          position: absolute;
-          width: 0px;
-          height: 0px;
-          left: 0px;
-          bottom: 0px;
-          content: "";
-          background: var(--app-header-selected-color);
-          opacity: 0.3;
-          transition: all 0.3s;
-        }
-
-        nav.pullUp a:hover:before,
-        nav.pullUp div.profile:hover:before {
-          width: 100%;
-          height: 2px;
-        }
-
-        nav div.profile:hover div.dropdown-content {
-          display: block;
-        }
-
-        nav.container {
-          /*font-family: Raleway;*/
-          margin: 0 auto;
-          /*padding: 10em 3em;*/
-          text-align: center;
-        }
-
-        .profile {
-          position: relative;
-        }
-
-        nav.container a,
-        nav.container div {
-          color: var(--app-header-text-color);
-          text-decoration: none;
-          font: 20px Ubuntu;
-          margin: 0px 10px;
-          padding: 10px 10px;
-          position: relative;
-          z-index: 0;
-          cursor: pointer;
-        }
-
-        .dropdown-content {
-          display: none;
-          position: absolute !important;
-          background-color: #f9f9f9;
-          margin-top: 25px;
-          min-width: 160px;
-          right: 0;
-          box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-          z-index: 1;
-        }
-
-        .dropdown-content a,
-        .dropdown-content div {
-          color: black;
-          padding: 12px 16px;
-          text-decoration: none;
-          display: block;
-        }
-
-        /* Wide layout: when the viewport width is bigger than 460px, layout
-      changes to a wide layout. */
-        @media (min-width: 460px) {
-          .toolbar-list {
-            display: block;
-          }
-
-          .menu-btn {
-            display: none;
-          }
-
-          .main-content {
-            padding-top: 107px;
-            min-height: calc(100vh - 212px);
-          }
-
-          /* The drawer button isn't shown in the wide layout, so we don't
-        need to offset the title */
-          [main-title] {
-            padding-right: 0px;
-          }
-        }
-      </style>
-
+      ${AppStyle} ${FooterStyle}
       <!-- Header -->
-      <app-header condenses reveals effects="waterfall">
+      <app-header fixed effects="waterfall">
         <app-toolbar class="toolbar-top">
           <button
             class="menu-btn"
@@ -288,31 +62,33 @@ class IEA extends connect(store)(LitElement) {
           >
             ${menuIcon}
           </button>
-          <div main-title>${this.appTitle}</div>
+          <div main-title>
+            <img src="../../images/Logo.png" style="height: 50px;" />${this
+              .appTitle}
+          </div>
         </app-toolbar>
-
         <!-- This gets hidden on a small screen-->
         <nav class="toolbar-list container pullUp">
           <a ?selected="${this._page === "Home"}" href="/Home" left-item
             ><iron-icon icon="home"></iron-icon>Home</a
           >
           <a ?selected="${this._page === "Payments"}" href="/Payments" left-item
-            >Payments</a
+            ><iron-icon icon="credit-card"></iron-icon>Payments</a
           >
           <a ?selected="${this._page === "Overview"}" href="/Overview" left-item
-            >Overview</a
+            ><iron-icon icon="inbox"></iron-icon>Overview</a
           >
           <a
             ?selected="${this._page === "Evaluation"}"
             href="/Evaluation"
             left-item
-            >Evaluation</a
+            ><iron-icon icon="editor:insert-chart"></iron-icon>Evaluation</a
           >
           <a ?selected="${this._page === "Login"}" href="/Login" right-item
             >Sign In/Sign Up</a
           >
           <div class="profile" right-item>
-            Profile
+            <iron-icon icon="social:person"></iron-icon>Profile
             <div class="dropdown-content">
               <a href="#">Link 1</a>
               <a href="#">Link 2</a>
@@ -321,7 +97,6 @@ class IEA extends connect(store)(LitElement) {
           </div>
         </nav>
       </app-header>
-
       <!-- Drawer content -->
       <app-drawer
         .opened="${this._drawerOpened}"
@@ -332,22 +107,22 @@ class IEA extends connect(store)(LitElement) {
             ><iron-icon icon="home"></iron-icon>Home</a
           >
           <a ?selected="${this._page === "Payments"}" href="/Payments" left-item
-            >Payments</a
+            ><iron-icon icon="credit-card"></iron-icon>Payments</a
           >
           <a ?selected="${this._page === "Overview"}" href="/Overview" left-item
-            >Overview</a
+            ><iron-icon icon="inbox"></iron-icon>Overview</a
           >
           <a
             ?selected="${this._page === "Evaluation"}"
             href="/Evaluation"
             left-item
-            >Evaluation</a
+            ><iron-icon icon="editor:insert-chart"></iron-icon>Evaluation</a
           >
-          <a ?selected="${this._page === "Login"}" href="/Login" right-item
+          <a ?selected="${this._page === "Login"}" href="/Login" left-item
             >Sign In/Sign Up</a
           >
-          <div class="profile" right-item>
-            Profile
+          <div class="profile" left-item>
+            <iron-icon icon="social:person"></iron-icon>Profile
             <div class="dropdown-content">
               <a href="#">Link 1</a>
               <a href="#">Link 2</a>
@@ -356,7 +131,6 @@ class IEA extends connect(store)(LitElement) {
           </div>
         </nav>
       </app-drawer>
-
       <!-- Main content -->
       <main role="main" class="main-content">
         <home-view class="page" ?active="${this._page === "Home"}"></home-view>
@@ -374,6 +148,7 @@ class IEA extends connect(store)(LitElement) {
         ></evaluation-view>
         <login-view
           class="page"
+          style="min-height: 630px;"
           ?active="${this._page === "Login"}"
         ></login-view>
         <my-view404
@@ -381,83 +156,23 @@ class IEA extends connect(store)(LitElement) {
           ?active="${this._page === "view404"}"
         ></my-view404>
       </main>
+      <footer class="footer-distributed">
+        <div class="footer-left">
+          <div style="display: -webkit-inline-flex;">
+            <img src="../../images/Logo.png" /><span>IEA</span>
+          </div>
+          <p class="footer-company-name">IEA &copy; 2019</p>
+        </div>
 
-      <footer>
-        <p>Footer</p>
+        <div class="footer-right">
+          <p>Contact Us:</p>
+          <form action="#" method="post">
+            <input type="text" name="email" placeholder="Email" />
+            <textarea name="message" placeholder="Message"></textarea>
+            <button>Send</button>
+          </form>
+        </div>
       </footer>
-
-      <snack-bar ?active="${this._snackbarOpened}">
-        You are now ${this._offline ? "offline" : "online"}.</snack-bar
-      >
-=======
-    ${AppStyle}
-    ${FooterStyle}
-
-    <!-- Header -->
-    <app-header fixed effects="waterfall">
-      <app-toolbar class="toolbar-top">
-        <button class="menu-btn" title="Menu" @click="${this._menuButtonClicked}">${menuIcon}</button>
-        <div main-title><img src="../../images/Logo.png" style="height: 50px;"/>${this.appTitle}</div>
-      </app-toolbar>
-
-      <!-- This gets hidden on a small screen-->
-      <nav class="toolbar-list container pullUp">
-        <a ?selected="${this._page === 'Home'}" href="/Home" left-item><iron-icon icon="home"></iron-icon>Home</a>
-        <a ?selected="${this._page === 'Payments'}" href="/Payments" left-item><iron-icon icon="credit-card"></iron-icon>Payments</a>
-        <a ?selected="${this._page === 'Overview'}" href="/Overview" left-item><iron-icon icon="inbox"></iron-icon>Overview</a>
-        <a ?selected="${this._page === 'Evaluation'}" href="/Evaluation" left-item><iron-icon icon="editor:insert-chart"></iron-icon>Evaluation</a>
-        <a ?selected="${this._page === 'Login'}" href="/Login" right-item>Sign In/Sign Up</a>
-        <div class="profile" right-item><iron-icon icon="social:person"></iron-icon>Profile<div class="dropdown-content">
-          <a href="#">Link 1</a>
-          <a href="#">Link 2</a>
-          <a href="#">Link 3</a>
-          </div></div>
-      </nav>
-    </app-header>
-
-    <!-- Drawer content -->
-    <app-drawer .opened="${this._drawerOpened}"
-        @opened-changed="${this._drawerOpenedChanged}">
-      <nav class="drawer-list container pullUp">
-        <a ?selected="${this._page === 'Home'}" href="/Home" left-item><iron-icon icon="home"></iron-icon>Home</a>
-        <a ?selected="${this._page === 'Payments'}" href="/Payments" left-item><iron-icon icon="credit-card"></iron-icon>Payments</a>
-        <a ?selected="${this._page === 'Overview'}" href="/Overview" left-item><iron-icon icon="inbox"></iron-icon>Overview</a>
-        <a ?selected="${this._page === 'Evaluation'}" href="/Evaluation" left-item><iron-icon icon="editor:insert-chart"></iron-icon>Evaluation</a>
-        <a ?selected="${this._page === 'Login'}" href="/Login" left-item>Sign In/Sign Up</a>
-        <div class="profile" left-item><iron-icon icon="social:person"></iron-icon>Profile<div class="dropdown-content">
-          <a href="#">Link 1</a>
-          <a href="#">Link 2</a>
-          <a href="#">Link 3</a>
-          </div></div>
-      </nav>
-    </app-drawer>
-
-    <!-- Main content -->
-    <main role="main" class="main-content">
-      <home-view class="page" ?active="${this._page === 'Home'}"></home-view>
-      <payments-view class="page" ?active="${this._page === 'Payments'}"></payments-view>
-      <overview-view class="page" ?active="${this._page === 'Overview'}"></overview-view>
-      <evaluation-view class="page" ?active="${this._page === 'Evaluation'}"></evaluation-view>
-      <login-view class="page" style="min-height: 630px;" ?active="${this._page === 'Login'}"></login-view>
-      <my-view404 class="page" ?active="${this._page === 'view404'}"></my-view404>
-    </main>
-
-    <footer class="footer-distributed">
-			<div class="footer-left">
-				<div style="display: -webkit-inline-flex;"><img src="../../images/Logo.png" /><span>IEA</span></div>
-				<p class="footer-company-name">IEA &copy; 2015</p>
-			</div>
-
-			<div class="footer-right">
-				<p>Contact Us:</p>
-				<form action="#" method="post">
-					<input type="text" name="email" placeholder="Email" />
-					<textarea name="message" placeholder="Message"></textarea>
-					<button>Send</button>
-				</form>
-			</div>
-		</footer>
->>>>>>> 236c719ad45f1b5f2d1db75fb4f22fc2043074cb
     `;
   }
 
@@ -477,7 +192,7 @@ class IEA extends connect(store)(LitElement) {
     // See https://www.polymer-project.org/3.0/docs/devguide/settings#setting-passive-touch-gestures
     // TODO: handle result
 
-    fetch("http://localhost:8080/iea/rs/auth/authenticate", {
+    fetch("http://localhost:8080/iea/api/auth/authenticate", {
       method: "post",
       credentials: "include"
     }).then(res => {
