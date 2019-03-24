638f0b6a82414cd0499340491876e26a7e3687dc
JulianNobis <julian09022001@gmail.com>
2019-03-24 21:42:38 +0100

    add additional data for automatic category assignment

--- JEE-Server/src/main/java/at/htl/iea/business/InitBean.java
+++ JEE-Server/src/main/java/at/htl/iea/business/InitBean.java
@@ -25,53 +25,118 @@ public class InitBean {
         System.out.println("********** INIT **********");
 
         Category wohnen = new Category("Wohnen");
+        Category gehalt = new Category("Gehalt");
         Category essen = new Category("Essen");
         Category strom = new Category("Strom");
         Category bank = new Category("Bank");
         Category auto = new Category("Auto");
         Category versicherung = new Category("Versicherung");
         Category tank = new Category("Tank");
+        Category kleidung = new Category("Kleidung");
         Category sonstiges = new Category("Sonstiges");
+        Category amazon = new Category("Amazon");
+        Category bildung = new Category("Bildung");
+
         auto.addSubcategory(versicherung);
         auto.addSubcategory(tank);
-
         wohnen.addSubcategory(strom);
         wohnen.addSubcategory(essen);
 
         em.persist(strom);
+        em.persist(gehalt);
         em.persist(essen);
         em.persist(wohnen);
         em.persist(bank);
         em.persist(versicherung);
         em.persist(tank);
         em.persist(auto);
+        em.persist(kleidung);
         em.persist(sonstiges);
+        em.persist(amazon);
+        em.persist(bildung);
 
         Assignment essenAssignment = new Assignment(essen);
         essenAssignment.addKeyword("Billa");
-        essenAssignment.addKeyword("Spar");
+        //essenAssignment.addKeyword("Spar"); //keyword nicht optimal -> check
         essenAssignment.addKeyword("Hofer");
+        essenAssignment.addKeyword("Penny");
+        essenAssignment.addKeyword("Unimarkt");
+        essenAssignment.addKeyword("Merkur");
+        essenAssignment.addKeyword("Lieferservice");
+        essenAssignment.addKeyword("mjam");
+        essenAssignment.addKeyword("Pizza");
+
+        Assignment gehaltAssignment = new Assignment(gehalt);
+        gehaltAssignment.addKeyword("Lohn");
+        gehaltAssignment.addKeyword("Gehalt");
 
         Assignment tankAssignment = new Assignment(tank);
         tankAssignment.addKeyword("Avanti");
         tankAssignment.addKeyword("Shell");
+        tankAssignment.addKeyword("BP");
+        tankAssignment.addKeyword("Avira");
+        tankAssignment.addKeyword("Tankstelle");
 
         Assignment wohnenAssignment = new Assignment(wohnen);
-        wohnenAssignment.addKeyword("TestW");
+        wohnenAssignment.addKeyword("IKEA");
+        wohnenAssignment.addKeyword("Möbelix");
+        wohnenAssignment.addKeyword("KIKA");
+        wohnenAssignment.addKeyword("Leiner");
+        wohnenAssignment.addKeyword("XXXLutz");
+        wohnenAssignment.addKeyword("Obi");
+        wohnenAssignment.addKeyword("Bauhaus");
+        wohnenAssignment.addKeyword("FIT-INN");
+        wohnenAssignment.addKeyword("FITINN");
+        wohnenAssignment.addKeyword("FIT INN");
+        wohnenAssignment.addKeyword("HIGH5");
+        wohnenAssignment.addKeyword("JOHN HARRIS");
+        wohnenAssignment.addKeyword("Fitness");
+        wohnenAssignment.addKeyword("clever fit");
+        wohnenAssignment.addKeyword("gym");
 
-        Assignment assignment1 = new Assignment(strom);
-        Assignment assignment2 = new Assignment(bank);
-        Assignment assignment3 = new Assignment(auto);
-        Assignment assignment4 = new Assignment(versicherung);
-        Assignment assignment5 = new Assignment(sonstiges);
+        Assignment stromAssignment = new Assignment(strom);
+        stromAssignment.addKeyword("Strom");
+        stromAssignment.addKeyword("Licht");
+
+        Assignment bankAssignment = new Assignment(bank);
+        bankAssignment.addKeyword("SB-Eigenerlag");
+        bankAssignment.addKeyword("SB-Auszahlung");
+        bankAssignment.addKeyword("Bank");
+        bankAssignment.addKeyword("Raiffeisen");
+        bankAssignment.addKeyword("Sparkasse");
+
+        Assignment autoAssignment = new Assignment(auto);
+        autoAssignment.addKeyword("Reifen");
+        autoAssignment.addKeyword("Leasing");
+
+        Assignment versicherungAssignment = new Assignment(versicherung);
+        versicherungAssignment.addKeyword("Versicherung");
+
+        Assignment kleidungAssignment = new Assignment(kleidung);
+        kleidungAssignment.addKeyword("Zalando");
+        kleidungAssignment.addKeyword("Kleidung");
+        kleidungAssignment.addKeyword("Asos");
+
+        Assignment amazonAssignment = new Assignment(amazon);
+        amazonAssignment.addKeyword("Amazon");
+
+        Assignment bildungAssignment = new Assignment(bildung);
+        bildungAssignment.addKeyword("Thalia");
+        bildungAssignment.addKeyword("Udemy");
+
+        Assignment sonstigesAssignment = new Assignment(sonstiges);
 
         em.persist(essenAssignment);
+        em.persist(gehaltAssignment);
         em.persist(tankAssignment);
         em.persist(wohnenAssignment);
-        em.persist(assignment1);
-        em.persist(assignment2);
-        em.persist(assignment3);
-        em.persist(assignment4);
-        em.persist(assignment5);
+        em.persist(stromAssignment);
+        em.persist(bankAssignment);
+        em.persist(autoAssignment);
+        em.persist(versicherungAssignment);
+        em.persist(kleidungAssignment);
+        em.persist(amazonAssignment);
+        em.persist(bildungAssignment);
+        em.persist(sonstigesAssignment);
     }
 }
