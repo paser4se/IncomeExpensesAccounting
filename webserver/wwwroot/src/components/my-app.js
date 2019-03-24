7ce33b75fcbace369fdcacc6d43dbc876a689d85
JulianNobis <julian09022001@gmail.com>
2019-03-24 14:37:12 +0100

    updating my-app.js

--- webserver/wwwroot/src/components/my-app.js
+++ webserver/wwwroot/src/components/my-app.js
@@ -8,18 +8,6 @@ Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
 */
 
-<<<<<<< HEAD
-/*import { LitElement, html } from "@polymer/lit-element";
-import { setPassiveTouchGestures } from "@polymer/polymer/lib/utils/settings.js";
-import { connect } from "pwa-helpers/connect-mixin.js";
-import { installMediaQueryWatcher } from "pwa-helpers/media-query.js";
-import { installOfflineWatcher } from "pwa-helpers/network.js";
-import { installRouter } from "pwa-helpers/router.js";
-import { updateMetadata } from "pwa-helpers/metadata.js";
-import { createStore } from "redux";*/
-
-import "@polymer/iron-icon/iron-icon.js";
-import "@polymer/iron-icons/iron-icons.js";
 import { LitElement, html } from "@polymer/lit-element";
 import { setPassiveTouchGestures } from "@polymer/polymer/lib/utils/settings.js";
 import { connect } from "pwa-helpers/connect-mixin.js";
@@ -27,29 +15,13 @@ import { installMediaQueryWatcher } from "pwa-helpers/media-query.js";
 import { installOfflineWatcher } from "pwa-helpers/network.js";
 import { installRouter } from "pwa-helpers/router.js";
 import { updateMetadata } from "pwa-helpers/metadata.js";
-=======
-import { LitElement, html } from '@polymer/lit-element';
-import { setPassiveTouchGestures } from '@polymer/polymer/lib/utils/settings.js';
-import { connect } from 'pwa-helpers/connect-mixin.js';
-import { installMediaQueryWatcher } from 'pwa-helpers/media-query.js';
-import { installOfflineWatcher } from 'pwa-helpers/network.js';
-import { installRouter } from 'pwa-helpers/router.js';
-import { updateMetadata } from 'pwa-helpers/metadata.js';
->>>>>>> 739c1a232cbc1d50fb1ef9cbad634734f85d661f
 import { FooterStyle } from "./footer-style";
 import { AppStyle } from "./app-style";
 
-<<<<<<< HEAD
 import "@polymer/iron-icon/iron-icon.js";
 import "@polymer/iron-icons/iron-icons.js";
 import "@polymer/iron-icons/editor-icons.js";
 import "@polymer/iron-icons/social-icons.js";
-=======
-import '@polymer/iron-icon/iron-icon.js';
-import '@polymer/iron-icons/iron-icons.js';
-import '@polymer/iron-icons/editor-icons.js';
-import '@polymer/iron-icons/social-icons.js';
->>>>>>> 739c1a232cbc1d50fb1ef9cbad634734f85d661f
 
 // This element is connected to the Redux store.
 import { store } from "../store.js";
@@ -68,8 +40,8 @@ class IEA extends connect(store)(LitElement) {
   render() {
     // Anything that's related to rendering should be done in here.
     return html`
-<<<<<<< HEAD
       ${AppStyle} ${FooterStyle}
+
       <!-- Header -->
       <app-header fixed effects="waterfall">
         <app-toolbar class="toolbar-top">
@@ -85,6 +57,7 @@ class IEA extends connect(store)(LitElement) {
               .appTitle}
           </div>
         </app-toolbar>
+
         <!-- This gets hidden on a small screen-->
         <nav class="toolbar-list container pullUp">
           <a ?selected="${this._page === "Home"}" href="/Home" left-item
@@ -115,6 +88,7 @@ class IEA extends connect(store)(LitElement) {
           </div>
         </nav>
       </app-header>
+
       <!-- Drawer content -->
       <app-drawer
         .opened="${this._drawerOpened}"
@@ -149,6 +123,7 @@ class IEA extends connect(store)(LitElement) {
           </div>
         </nav>
       </app-drawer>
+
       <!-- Main content -->
       <main role="main" class="main-content">
         <home-view class="page" ?active="${this._page === "Home"}"></home-view>
@@ -174,6 +149,7 @@ class IEA extends connect(store)(LitElement) {
           ?active="${this._page === "view404"}"
         ></my-view404>
       </main>
+
       <footer class="footer-distributed">
         <div class="footer-left">
           <div style="display: -webkit-inline-flex;">
@@ -191,75 +167,6 @@ class IEA extends connect(store)(LitElement) {
           </form>
         </div>
       </footer>
-=======
-    ${AppStyle}
-    ${FooterStyle}
-
-    <!-- Header -->
-    <app-header fixed effects="waterfall">
-      <app-toolbar class="toolbar-top">
-        <button class="menu-btn" title="Menu" @click="${this._menuButtonClicked}">${menuIcon}</button>
-        <div main-title><img src="../../images/Logo.png" style="height: 50px;"/>${this.appTitle}</div>
-      </app-toolbar>
-
-      <!-- This gets hidden on a small screen-->
-      <nav class="toolbar-list container pullUp">
-        <a ?selected="${this._page === 'Home'}" href="/Home" left-item><iron-icon icon="home"></iron-icon>Home</a>
-        <a ?selected="${this._page === 'Payments'}" href="/Payments" left-item><iron-icon icon="credit-card"></iron-icon>Payments</a>
-        <a ?selected="${this._page === 'Overview'}" href="/Overview" left-item><iron-icon icon="inbox"></iron-icon>Overview</a>
-        <a ?selected="${this._page === 'Evaluation'}" href="/Evaluation" left-item><iron-icon icon="editor:insert-chart"></iron-icon>Evaluation</a>
-        <a ?selected="${this._page === 'Login'}" href="/Login" right-item>Sign In/Sign Up</a>
-        <div class="profile" right-item><iron-icon icon="social:person"></iron-icon>Profile<div class="dropdown-content">
-          <a href="#">Link 1</a>
-          <a href="#">Link 2</a>
-          <a href="#">Link 3</a>
-          </div></div>
-      </nav>
-    </app-header>
-
-    <!-- Drawer content -->
-    <app-drawer .opened="${this._drawerOpened}"
-        @opened-changed="${this._drawerOpenedChanged}">
-      <nav class="drawer-list container pullUp">
-        <a ?selected="${this._page === 'Home'}" href="/Home" left-item><iron-icon icon="home"></iron-icon>Home</a>
-        <a ?selected="${this._page === 'Payments'}" href="/Payments" left-item><iron-icon icon="credit-card"></iron-icon>Payments</a>
-        <a ?selected="${this._page === 'Overview'}" href="/Overview" left-item><iron-icon icon="inbox"></iron-icon>Overview</a>
-        <a ?selected="${this._page === 'Evaluation'}" href="/Evaluation" left-item><iron-icon icon="editor:insert-chart"></iron-icon>Evaluation</a>
-        <a ?selected="${this._page === 'Login'}" href="/Login" left-item>Sign In/Sign Up</a>
-        <div class="profile" left-item><iron-icon icon="social:person"></iron-icon>Profile<div class="dropdown-content">
-          <a href="#">Link 1</a>
-          <a href="#">Link 2</a>
-          <a href="#">Link 3</a>
-          </div></div>
-      </nav>
-    </app-drawer>
-
-    <!-- Main content -->
-    <main role="main" class="main-content">
-      <home-view class="page" ?active="${this._page === 'Home'}"></home-view>
-      <payments-view class="page" ?active="${this._page === 'Payments'}"></payments-view>
-      <overview-view class="page" ?active="${this._page === 'Overview'}"></overview-view>
-      <evaluation-view class="page" ?active="${this._page === 'Evaluation'}"></evaluation-view>
-      <login-view class="page" style="min-height: 630px;" ?active="${this._page === 'Login'}"></login-view>
-      <my-view404 class="page" ?active="${this._page === 'view404'}"></my-view404>
-    </main>
-
-    <footer class="footer-distributed">
-        <div class="footer-left">
-            <div style="display: -webkit-inline-flex;"><img src="../../images/Logo.png" /><span>IEA</span></div>
-            <p class="footer-company-name">IEA &copy; 2019</p>
-        </div>
-  
-        <div class="footer-right">
-            <p>Contact Us:</p>
-            <form action="#" method="post">
-                <input type="text" name="email" placeholder="Email" />
-                <textarea name="message" placeholder="Message"></textarea>
-                <button>Send</button>
-            </form>
-        </div>
-    </footer>
->>>>>>> 739c1a232cbc1d50fb1ef9cbad634734f85d661f
     `;
   }
 
