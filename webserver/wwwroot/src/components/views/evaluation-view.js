e28fb7b3995678a1b9e985584e3c7541d74c2b3e
JulianNobis <julian09022001@gmail.com>
2019-03-25 09:25:48 +0100

    added first part of pie chart (dummy values)

--- webserver/wwwroot/src/components/views/evaluation-view.js
+++ webserver/wwwroot/src/components/views/evaluation-view.js
@@ -3,6 +3,8 @@ import { PageViewElement } from "../page-view-element.js";
 
 import { SharedStyles } from "../shared-styles";
 
+import Chart from "chart.js";
+
 class EvaluationView extends PageViewElement {
   render() {
     return html`
@@ -10,35 +12,38 @@ class EvaluationView extends PageViewElement {
 
       <section>
         <div>Evaluation</div>
-        <div id="container" style="width: 100%; height: 100%"></div>
+        <canvas id="pie-chart" width="800" height="450"></canvas>
       </section>
     `;
+  }
+  connectedCallback() {
+    console.log(Chart);
+    new Chart(this.shadowRoot.getElementById("pie-chart"), {
+      type: "pie",
+      data: {
+        labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+        datasets: [
+          {
+            label: "Population (millions)",
+            backgroundColor: [
+              "#3e95cd",
+              "#8e5ea2",
+              "#3cba9f",
+              "#e8c3b9",
+              "#c45850"
+            ],
+            data: [2478, 5267, 734, 784, 433]
+          }
+        ]
+      },
+      options: {
+        title: {
+          display: true,
+          text: "Predicted world population (millions) in 2050"
+        }
+      }
+    });
   }
 }
 
 window.customElements.define("evaluation-view", EvaluationView);
-anychart.onDocumentReady(function() {
-  // dummy werte
-  var data = [
-    { x: "Gehalt", value: 10000 },
-    { x: "Sonstiges", value: 4500 },
-    { x: "Bildung", value: 300 },
-    { x: "Wohnen", value: 2500 },
-    { x: "Kleidung", value: 150 },
-    { x: "Essen", value: 1500 },
-    { x: "Auto", value: 1000 }
-  ];
-
-  // create the chart
-  var chart = anychart.pie();
-
-  // set the chart title
-  chart.title("Auswertung Ihrer Zahlungen");
-
-  // add the data
-  chart.data(data);
-
-  // display the chart in the container
-  chart.container("container");
-  chart.draw();
-});
