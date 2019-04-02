78298bba8ddba1e36cbcfb3c0bf73b49a2661628
JulianNobis <julian09022001@gmail.com>
2019-04-02 12:10:19 +0200

    included first prototype for evaluation

--- webserver/wwwroot/src/components/views/evaluation-view.js
+++ webserver/wwwroot/src/components/views/evaluation-view.js
@@ -3,36 +3,25 @@ import { PageViewElement } from "../page-view-element.js";
 
 import { SharedStyles } from "../shared-styles";
 
-import '@google-web-components/google-chart/google-chart.js';
-
-import { PolymerElement, html } from "@polymer/polymer";
 import "@google-web-components/google-chart/google-chart.js";
 
 class EvaluationView extends PageViewElement {
-
   render() {
     return html`
-      <style>
-      
-      </style>
+      <style></style>
       ${SharedStyles}
 
-<<<<<<< HEAD
-      <google-chart data='[["Month", "Days"], ["Jan", 31]]'></google-chart>
-=======
       <section>
-        <div>Evaluation</div>
         <google-chart
-          type='pie'
+          type="pie"
           options='{"width": "400", "height": "400", "title": "Test Chart", "chartArea": {"width": "300", "height": "300"}, "legend": "none"}'
           cols='[{"label":"Month", "type":"string"}, {"label":"Days", "type":"number"}]'
-          rows='[["Jan", 31],["Feb", 28],["Mar", 31]]'>
+          rows='[["Jan", 31],["Feb", 28],["Mar", 31]]'
+        >
         </google-chart>
       </section>
->>>>>>> 17da7fe25e74f4dfc8a85a4e2649d019ae3a7d82
     `;
   }
-
 }
 
 window.customElements.define("evaluation-view", EvaluationView);
