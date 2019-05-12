29833b993e0d78a307723d39ce6aee0bb7b6942d
JulianNobis <julian09022001@gmail.com>
2019-05-12 20:00:35 +0200

    added functionality for pie chart

--- webserver/wwwroot/src/components/views/evaluation-view.js
+++ webserver/wwwroot/src/components/views/evaluation-view.js
@@ -3,14 +3,22 @@ import { PageViewElement } from "../page-view-element.js";
 
 import { SharedStyles } from "../shared-styles";
 
-import '@google-web-components/google-chart/google-chart.js';
-import '@polymer/iron-icon/iron-icon.js';
-import '@polymer/iron-icons/iron-icons.js';
+import "@google-web-components/google-chart/google-chart.js";
+import "@polymer/iron-icon/iron-icon.js";
+import "@polymer/iron-icons/iron-icons.js";
 
-const rowValues = [["Gehalt", 31], ["Auto", 28], ["Wohnen", 31]]; // momentan noch dummy data
+//const rowValues = [["Gehalt", 31], ["Auto", 28], ["Wohnen", 31]]; // momentan noch dummy data
+const url = "http://localhost:8080/iea/api/evaluation/getAll";
 
 class EvaluationView extends PageViewElement {
+  async connectedCallback() {
+    this.rowValues = await fetch(url)
+      .then(res => res.json())
+      .then(json => json.map(Object.values));
+  }
+
   render() {
+    console.log(this.rowValues);
     return html`
       <style>
         .grid-container {
@@ -20,48 +28,48 @@ class EvaluationView extends PageViewElement {
           grid-template-columns: 20% 45% auto;
           grid-template-rows: 550px auto;
           grid-template-areas:
-          "filter chart table"
-          "expanded expanded expanded"
+            "filter chart table"
+            "expanded expanded expanded";
         }
-        
+
         .header {
           background-color: #288b9e;
           height: 50px;
-          display: flex; 
+          display: flex;
           display: -webkit-flex;
           vertical-align: center;
         }
-        
+
         .header-text {
-          text-align: center; 
+          text-align: center;
           margin: auto;
           color: white;
           font-weight: bold;
         }
-        
+
         .windowcontent {
-          
         }
-        
+
         #filterWindow {
           grid-area: filter;
-          box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19);
+          box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2),
+            0 6px 20px 0 rgba(0, 0, 0, 0.19);
         }
-        
+
         #chart {
           grid-area: chart;
         }
-        
+
         #table {
           grid-area: table;
           background-color: #39bfd3;
         }
-        
+
         #expanded {
           grid-area: expanded;
           background-color: goldenrod;
         }
-        
+
         .collapsible {
           background-color: #777;
           color: white;
@@ -73,11 +81,12 @@ class EvaluationView extends PageViewElement {
           outline: none;
           font-size: 15px;
         }
-        
-        .active, .collapsible:hover {
+
+        .active,
+        .collapsible:hover {
           background-color: #555;
         }
-        
+
         .content {
           padding: 0 18px;
           max-height: 0;
@@ -85,61 +94,57 @@ class EvaluationView extends PageViewElement {
           transition: max-height 0.2s ease-out;
           background-color: #f1f1f1;
         }
-        
+
         button::-moz-focus-inner {
           border: 0;
         }
       </style>
       ${SharedStyles}
 
-<<<<<<< HEAD
-      <section>
-        <google-chart
-          type="pie"
-          options='{"width": "400", "height": "400", "title": "Einnahmen & Ausgaben", "chartArea": {"width": "300", "height": "300"}, "legend": "none"}'
-          cols='[{"label":"Category", "type":"string"}, {"label":"Amount", "type":"number"}]'
-          rows=${JSON.stringify(rowValues)}
-        >
-        </google-chart>
-      </section>
-=======
       <div style="height: auto;">
         <div class="grid-container">
           <div id="filterWindow">
-              <div class="header"><label class="header-text">FILTER</label></div>
-              <div class="windowcontent">
-                <label>Comming soon....</label>
-              </div>
+            <div class="header"><label class="header-text">FILTER</label></div>
+            <div class="windowcontent">
+              <label>Coming soon....</label>
+            </div>
           </div>
           <div id="chart">
-          <google-chart type='pie'
-            options='{"width": "400", "height": "400", "title": "Test Chart", "chartArea": {"width": "300", "height": "300"}, "legend": "none"}'
-            cols='[{"label":"Month", "type":"string"}, {"label":"Days", "type":"number"}]'
-            rows='[["Jan", 31],["Feb", 28],["Mar", 31]]'>
-          </google-chart>
+            <google-chart
+              type="pie"
+              options='{"width": "400", "height": "400", "title": "Test Chart", "chartArea": {"width": "300", "height": "300"}, "legend": "none"}'
+              cols='[{"label":"Month", "type":"string"}, {"label":"Days", "type":"number"}]'
+              rows=${JSON.stringify(this.rowValues)}
+            >
+            </google-chart>
           </div>
           <div id="table">
-              <label>TABLE</label>
+            <label>TABLE</label>
           </div>
-          
+
           <div id="expanded">
-            <button class="collapsible" @click="${(evt) => this.toggle(evt)}">
-                <iron-icon id="icon_down" icon="arrow-drop-down"></iron-icon>
-                <iron-icon id="icon_up" icon="arrow-drop-up" style="display: none;"></iron-icon> Erweitert</button>
-             <div class="content">
-               <p>This is a placeholder....</p>
-             </div>
+            <button class="collapsible" @click="${evt => this.toggle(evt)}">
+              <iron-icon id="icon_down" icon="arrow-drop-down"></iron-icon>
+              <iron-icon
+                id="icon_up"
+                icon="arrow-drop-up"
+                style="display: none;"
+              ></iron-icon>
+              Erweitert
+            </button>
+            <div class="content">
+              <p>This is a placeholder....</p>
+            </div>
           </div>
         </div>
       </div>
->>>>>>> 573617da005b3ff5306b92aff287cec0102a9c4d
     `;
   }
 
   toggle(evt) {
-    var btn = this.shadowRoot.querySelector('.collapsible');
-    var icon_down = this.shadowRoot.querySelector('#icon_down');
-    var icon_up = this.shadowRoot.querySelector('#icon_up');
+    var btn = this.shadowRoot.querySelector(".collapsible");
+    var icon_down = this.shadowRoot.querySelector("#icon_down");
+    var icon_up = this.shadowRoot.querySelector("#icon_up");
     btn.classList.toggle("active");
     if (btn.classList.contains("active")) {
       icon_down.style.display = "none";
@@ -149,13 +154,12 @@ class EvaluationView extends PageViewElement {
       icon_down.style.display = "inherit";
     }
     var content = btn.nextElementSibling;
-    if (content.style.maxHeight){
+    if (content.style.maxHeight) {
       content.style.maxHeight = null;
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
     }
   }
-
 }
 
 window.customElements.define("evaluation-view", EvaluationView);
