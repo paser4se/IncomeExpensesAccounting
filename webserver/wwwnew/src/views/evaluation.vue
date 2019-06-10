40e081763298f723b4df809286691a068bbcf6ce
MichiFrech <michifrech2000@gmail.com>
2019-06-10 19:19:05 +0200

    Added evaluation demo

--- webserver/wwwnew/src/views/evaluation.vue
+++ webserver/wwwnew/src/views/evaluation.vue
@@ -1,32 +1,38 @@
 <template>
-  <dx-pie-chart
-    id="pie"
-    :data-source="areas"
-    palette="Bright"
-    title="Area of Countries"
-    @point-click="pointClickHandler($event)"
-    @legend-click="legendClickHandler($event)"
-  >
-    <dx-series argument-field="country" value-field="area">
-      <dx-label :visible="true">
-        <dx-connector :visible="true" :width="1"/>
-      </dx-label>
-    </dx-series>
-    <dx-size :width="500"/>
-    <dx-export :enabled="true"/>
-  </dx-pie-chart>
+  <div>
+    <h2 class="content-block">Evaluation</h2>
+    <div class="content-block">
+      <div class="dx-card responsive-paddings">
+        <dx-pie-chart
+          id="pie"
+          :data-source="areas"
+          palette="Bright"
+          title="Area of Countries"
+          @point-click="pointClickHandler($event)"
+          @legend-click="legendClickHandler($event)"
+        >
+          <dx-series argument-field="country" value-field="area">
+            <dx-label :visible="true">
+              <dx-connector :visible="true" :width="1"/>
+            </dx-label>
+          </dx-series>
+          <dx-size :width="500"/>
+          <dx-export :enabled="true"/>
+        </dx-pie-chart>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import DxPieChart, {
+import {
+  DxPieChart,
   DxSize,
   DxSeries,
   DxLabel,
   DxConnector,
   DxExport
 } from "devextreme-vue/pie-chart";
-
-import { areas } from "./data.js";
 
 export default {
   components: {
@@ -39,7 +45,40 @@ export default {
   },
   data() {
     return {
-      areas
+      areas: [
+        {
+          country: "Russia",
+          area: 12
+        },
+        {
+          country: "Canada",
+          area: 7
+        },
+        {
+          country: "USA",
+          area: 7
+        },
+        {
+          country: "China",
+          area: 7
+        },
+        {
+          country: "Brazil",
+          area: 6
+        },
+        {
+          country: "Australia",
+          area: 5
+        },
+        {
+          country: "India",
+          area: 2
+        },
+        {
+          country: "Others",
+          area: 55
+        }
+      ]
     };
   },
   methods: {
