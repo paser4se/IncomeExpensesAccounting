8f7be4d0b45f7a800025ab2a817f0283f59d3e41
JulianNobis <julian09022001@gmail.com>
2019-06-10 19:29:34 +0200

    old changes, should be overritten

--- webserver/wwwnew/src/views/evaluation.vue
+++ webserver/wwwnew/src/views/evaluation.vue
@@ -1,70 +1,31 @@
 <template>
-  <dx-pie-chart
-    id="pie"
-    :data-source="areas"
-    palette="Bright"
-    title="Area of Countries"
-    @point-click="pointClickHandler($event)"
-    @legend-click="legendClickHandler($event)"
-  >
-    <dx-series argument-field="country" value-field="area">
-      <dx-label :visible="true">
-        <dx-connector :visible="true" :width="1"/>
-      </dx-label>
-    </dx-series>
-    <dx-size :width="500"/>
-    <dx-export :enabled="true"/>
-  </dx-pie-chart>
+  <div>
+    <chartjs-dougnut v-bind:labels="labels" v-bind:option="option"></chartjs-dougnut>
+  </div>
 </template>
 
 <script>
-import DxPieChart, {
-  DxSize,
-  DxSeries,
-  DxLabel,
-  DxConnector,
-  DxExport
-} from "devextreme-vue/pie-chart";
-
-import { areas } from "./data.js";
-
 export default {
-  components: {
-    DxPieChart,
-    DxSize,
-    DxSeries,
-    DxLabel,
-    DxConnector,
-    DxExport
-  },
   data() {
     return {
-      areas
+      labels: ["Apples", "Bananas", "Grapes"],
+      datasets: [
+        {
+          data: [20, 30, 50],
+          backgroundColor: ["Red", "Yellow", "Purple"]
+        }
+      ],
+      option: {
+        title: {
+          display: true,
+          position: "bottom",
+          text: "Fruits"
+        }
+      }
     };
-  },
-  methods: {
-    pointClickHandler(e) {
-      this.toggleVisibility(e.target);
-    },
-    legendClickHandler(e) {
-      let arg = e.target,
-        item = e.component.getAllSeries()[0].getPointsByArg(arg)[0];
-
-      this.toggleVisibility(item);
-    },
-    toggleVisibility(item) {
-      item.isVisible() ? item.hide() : item.show();
-    }
   }
 };
 </script>
 
-<style>
-#pie {
-  height: 440px;
-}
-
-#pie * {
-  margin: 0 auto;
-}
+<style scoped>
 </style>
